7d0ea63c54d45b66a8abe46f1ecceebee675d1ac
chaosannals <chaosannals@outlook.com>
2020-08-26 10:56:41 +0800

    good luck.

---
+++ main/loop.js
@@ -0,0 +1,29 @@
+const three = require('three')
+
+const camera = new three.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10
+);
+camera.position.z = 1;
+const geometry = new three.BoxGeometry(0.2, 0.2, 0.2);
+const material = new three.MeshNormalMaterial();
+const mesh = new three.Mesh(geometry, material);
+const scene = new three.Scene();
+scene.add(mesh);
+
+const renderer = new three.WebGLRenderer({
+    antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+    requestAnimationFrame(animate);
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+    renderer.render(scene, camera);
+}
+
+animate();
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+function getAllEntry() {
+    const mainPath = path.resolve(__dirname, 'main');
+    const paths = fs.readdirSync(mainPath);
+    const result = {};
+    for (let filename of paths) {
+        let name = path.basename(filename, '.js');
+        result[name] = path.resolve(mainPath, filename);
+    }
+    console.log(result);
+    return result;
+}
+
+module.exports = {
+    entry: getAllEntry(),
+    output: {
+        library: '[name]',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    performance: {
+        hints: 'warning',
+        maxEntrypointSize: 4000000,
+        maxAssetSize: 2000000,
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 33333,
+        contentBase: [
+            path.resolve(__dirname, 'demo')
+        ],
+        contentBasePublicPath: [
+            '/'
+        ],
+    },
+};
